d577fc836f5d2f01a58bfe83d567112e17b00edd
waltermaffy <waltermaffy@yahoo.it>
2022-03-12 15:41:56 +0100

    install docker script

--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-#  update, upgrade and install docker
-sudo apt-get update && sudo apt-get upgrade
-curl -sSL https://get.docker.com | sh
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Docker already installed"
+  else
+    echo "Installing docker"
     
-# Add current user to docker group
-sudo usermod -aG docker ${USER}
+    # update, upgrade and install docker
+    sudo apt-get update && sudo apt-get upgrade
+    echo "Getting docker"
+    curl -sSL https://get.docker.com | sh
+        
+    # Add current user to docker group
+    sudo usermod -aG docker ${USER}
 
-# Install python, pip and docker-compose
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
-sudo pip3 install docker-compose
-# Enable the Docker system service to start your containers on boot
-sudo systemctl enable docker
+    # Install python, pip and docker-compose
+    sudo apt-get install libffi-dev libssl-dev
+    sudo apt install python3-dev
+    sudo apt-get install -y python3 python3-pip
+    sudo pip3 install docker-compose
+    # Enable the Docker system service to start your containers on boot
+    sudo systemctl enable docker
+fi
